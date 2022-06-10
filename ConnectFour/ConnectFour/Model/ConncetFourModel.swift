b39affcd552b6448199780bb933034c7d984cc47
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-06-10 14:31:21 -0500

    Update ConncetFourModel.swift

--- ConnectFour/ConnectFour/Model/ConncetFourModel.swift
+++ ConnectFour/ConnectFour/Model/ConncetFourModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct Conn4 {
+    private var piecesBox: Set<Piece> = [Piece(col: 0, row: 0, player: .yellow)]
+    
+    func dropAt(col: Int){  }
+    
+    func piencesAt(col: Int, row: Int) -> Piece? {
+        for piece in piecesBox {
+            if piece.col == col && piece.row == row {
+                return piece
+            }
+        }
+        return nil
+    }
+    
+    enum Player {
+        case red
+        case yellow
+    }
+    
+    struct Piece : Hashable {
+        var col : Int
+        var row : Int
+        var player : Player
+    }
+}
