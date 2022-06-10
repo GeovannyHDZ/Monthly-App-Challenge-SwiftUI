86ae5db21cb59db741773c96b7ee5361bb45c53f
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-06-10 10:49:10 -0500

    Update ContentView.swift

--- ConnectFour/ConnectFour/ContentView.swift
+++ ConnectFour/ConnectFour/ContentView.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Board().stroke(Color.red, lineWidth: 1)
+            Board().stroke(Color.cyan, lineWidth: 2)
+            Board().stroke(Color.pink, lineWidth: 3)
+            Board().stroke(Color.purple, lineWidth: 4)
+        }
     }
+}
+
+// Create an Struct for create an shape
+struct Board: Shape{
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        // Horizontal Lines
+        for row in 0..<6{
+            path.move(to: CGPoint(x: 20, y: 30 + row * 20))
+            path.addLine(to: CGPoint(x: 140, y: 30 + row * 20))
+        }
+        
+        // Vertical Lines
+        for colum in 0..<7{
+            path.move(to: CGPoint(x: 20 + colum * 20, y: 30))
+            path.addLine(to: CGPoint(x: 20 + colum * 20, y: 30 + 5 * 20))
+        }
+        return path
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
