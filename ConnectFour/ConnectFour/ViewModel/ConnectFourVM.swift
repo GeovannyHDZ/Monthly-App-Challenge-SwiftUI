e00aaa3ecd0dfa9433bbd37f354e096f4fdfa94d
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-06-10 14:31:26 -0500

    Update ConnectFourVM.swift

--- ConnectFour/ConnectFour/ViewModel/ConnectFourVM.swift
+++ ConnectFour/ConnectFour/ViewModel/ConnectFourVM.swift
@@ -5,4 +5,16 @@
 //  Created by Angel Hernández Alegría on 10/06/22.
 //
 
-import Foundation
+import SwiftUI
+
+class Conn4VM: ObservableObject{
+    @Published private var game = Conn4()
+    
+    func piencesAt(col: Int, row: Int) -> Conn4.Piece? {
+        game.piencesAt(col: col, row: row)
+    }
+    
+    func dropAt(col: Int) {
+        game.dropAt(col: col)
+    }
+}
